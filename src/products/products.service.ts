df2a5e08e5ec6cda3e42704bbc388f2b595b5995
FDMC04 <franciscodanielmzca@gmail.com>
2026-03-22 17:44:50 -0600

    PaginarEnTypeORM

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -9,6 +9,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 export class ProductsService {
   private readonly logger = new Logger('ProductsService');
@@ -39,8 +40,12 @@ export class ProductsService {
     }
   }
 
-  async findAll() {
-    return this.productRepository.find({});
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    return this.productRepository.find({
+      take: limit,
+      skip: offset,
+    });
   }
 
   async findOne(id: string) {
