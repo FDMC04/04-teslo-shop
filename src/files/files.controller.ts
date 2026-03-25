69a86720f5b52278fb88f5d25283a8cdb8bac36f
FDMC04 <franciscodanielmzca@gmail.com>
2026-03-24 18:58:39 -0600

    GuardarImagenEnFilesystem

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -8,13 +8,21 @@ import {
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file', { fileFilter: fileFilter }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      /*limits: { fileSize:1000  } */ storage: diskStorage({
+        destination: './static/uploads',
+      }),
+    }),
+  )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
       throw new BadRequestException('Make sure that file is an image');
