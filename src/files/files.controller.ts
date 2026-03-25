7fdf70b6f8f2c0843345115d7198c9de2d57327f
FDMC04 <franciscodanielmzca@gmail.com>
2026-03-24 19:44:17 -0600

    ServirArchivosDeManeraControlada

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,7 +1,10 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
@@ -9,10 +12,21 @@ import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileNamer, fileFilter } from './helpers';
+import type { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    // res.status(403).json({ ok: false, path: path });
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -28,6 +42,8 @@ export class FilesController {
     if (!file) {
       throw new BadRequestException('Make sure that file is an image');
     }
-    return { fileName: file.originalname };
+    // const secureUrl = `${file.filename}`;
+    const secureUrl = `http://localhost:3000/api/files/product/977f8841-6523-4dc7-ada7-19b0af30eb7c.png`;
+    return { secureUrl };
   }
 }
