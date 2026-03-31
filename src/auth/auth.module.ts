649b6bfac6a46f696469bf1b52ce7513ff1f4bdf
FDMC04 <franciscodanielmzca@gmail.com>
2026-03-30 18:37:46 -0600

    MódulosAsíncronos

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,7 +4,8 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
-// import { JwtModule } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
@@ -12,6 +13,18 @@ import { PassportModule } from '@nestjs/passport';
   imports: [
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // console.log('JWT_SECRET', configService.get('JWT_SECRET'));
+        // console.log('JWT_SECRET', process.env.JWT_SECRET);
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '2h' },
+        };
+      },
+    }),
     // JwtModule.register({ secret: process.env.JWT_SECRET, signOptions: { expiresIn: '2h' } }),
   ],
   exports: [TypeOrmModule],
