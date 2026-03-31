70b1c1e27a93a2e4f895076520773734a2e20f7e
FDMC04 <franciscodanielmzca@gmail.com>
2026-03-31 15:13:37 -0600

    Tarea-CustomDecorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Req,
+  Headers,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
+import { GetUser, RawHeaders } from './decorators';
+import type { IncomingHttpHeaders } from 'http';
 
 @Controller('auth')
 export class AuthController {
@@ -22,14 +31,21 @@ export class AuthController {
   @Get('private')
   @UseGuards(AuthGuard())
   testingPrivateRoute(
-    /*@Req() request: Express.Request*/ @GetUser() user: User,
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[],
+    @Headers() headers: IncomingHttpHeaders,
   ) {
-    // console.log(request);
+    console.log(request);
     // console.log(user);
     return {
       oK: true,
       message: 'Hola mundo',
       user,
+      userEmail,
+      rawHeaders,
+      headers,
     };
   }
 }
--- src/auth/decorators/get-user.decorator.ts
+++ src/auth/decorators/get-user.decorator.ts
@@ -1,8 +1,16 @@
-import { createParamDecorator, ExecutionContext, InternalServerErrorException } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 export const GetUser = createParamDecorator((data, ctx: ExecutionContext) => {
   const req = ctx.switchToHttp().getRequest();
+  //   if (data) {
+  //     const user = req.user[data];
+  //     return user;
+  //   }
   const user = req.user;
   if (!user) throw new InternalServerErrorException('User not found (request)');
-  return user;
+  return !data ? user : user[data];
 });
