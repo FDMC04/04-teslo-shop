4188dae8dd9eb20f6ae1bcda4e6ee11967c7c47f
FDMC04 <franciscodanielmzca@gmail.com>
2026-03-31 14:22:40 -0600

    CustomPropertyDecorator-GetUser

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -19,11 +21,15 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute() {
+  testingPrivateRoute(
+    /*@Req() request: Express.Request*/ @GetUser() user: User,
+  ) {
+    // console.log(request);
+    // console.log(user);
     return {
       oK: true,
       message: 'Hola mundo',
-      user: 'Francisco',
+      user,
     };
   }
 }
