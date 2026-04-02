db3a5d444efed98b05a3a99e61b02a7381210257
FDMC04 <franciscodanielmzca@gmail.com>
2026-04-02 13:28:22 -0600

    Cliente-ClientesConectados

--- src/messsages-ws/messsages-ws.gateway.ts
+++ src/messsages-ws/messsages-ws.gateway.ts
@@ -2,22 +2,33 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { MesssagesWsService } from './messsages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
 export class MesssagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer() wss: Server;
+
   constructor(private readonly messsagesWsService: MesssagesWsService) {}
 
   handleConnection(client: Socket) {
     // console.log('Cliente conectado:', client.id);
     this.messsagesWsService.registerClient(client);
+    this.wss.emit(
+      'clients-updated',
+      this.messsagesWsService.getConnectedClients(),
+    );
   }
 
   handleDisconnect(client: Socket) {
     this.messsagesWsService.removeClient(client.id);
+    this.wss.emit(
+      'clients-updated',
+      this.messsagesWsService.getConnectedClients(),
+    );
   }
 }
